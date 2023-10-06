454dd3dcf08a2323b282a1c15de572cea63ec530
Naveena2906 <727821tucs138@skct.edu.in>
2023-10-06 12:22:25 +0530

    spring security

---
+++ day 13/src/main/java/com/naveena/stockmanagement/dto/Response/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.naveena.stockmanagement.dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
